409dcb75f15d0860abb6a9da652d40ff794ec31d
Rodrigo Jeronimo <86856395+TheRodrig0@users.noreply.github.com>
2026-06-15 09:45:07 -0300

    feat: implement authentication routes, user model, and link redirection service

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -10,13 +10,14 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory;
+    use HasApiTokens, HasFactory;
 
     /**
      * Get the attributes that should be cast.
