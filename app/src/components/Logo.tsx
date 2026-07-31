fa27a732b95187c32c66abe7d3dd6c3f60625d6e
Rodrigo Jeronimo <86856395+TheRodrig0@users.noreply.github.com>
2026-07-31 18:12:40 -0300

    feat(app): implement core layout components and landing page structure

---
+++ app/src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import urlLogo from '../assets/logo_tramita.svg';
+
+const Logo = () => {
+    return (
+        <img src={urlLogo} alt="Tramita Logo" className="h-8 w-auto" />
+    );
+};
+
+export default Logo;
