761ad514b7307a389631410be112424cb9905c47
Rodrigo Jeronimo <86856395+TheRodrig0@users.noreply.github.com>
2026-08-01 03:38:10 -0300

    style(layout): improve mobile responsiveness for layout components

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,7 +1,27 @@
+import Logo from "./Logo"
+
 const Footer = () => {
+    const year = new Date().getFullYear()
+
     return (
         <>
-            <footer>
+            <footer className='relative flex flex-col md:flex-row justify-between items-center py-8 px-6 md:px-10 gap-6 md:gap-0'>
+                <div className='flex items-center gap-4'>
+                    <Logo className="brightness-0 h-6" />
+                    <p className='font-black text-xl tracking-tighter'>
+                        Tramita
+                    </p>
+                </div>
+
+                <p className="text-slate-800 text-sm md:text-base md:absolute md:left-1/2 md:-translate-x-1/2 text-center">
+                    @{year} Tramita. Todos direitos reservados.
+                </p>
+
+                <div className='flex gap-4 md:gap-2 text-sm md:text-base'>
+                    <a href="">Termos</a>
+                    <a href="">Privacidade</a>
+                    <a href="https://github.com/TheRodrig0/Tramita">GitHub</a>
+                </div>
             </footer>
         </>
     )
--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -3,19 +3,17 @@ import Logo from "./Logo"
 
 const Header = () => {
     return (
-        <>
-            <header className=">sticky top-0 z-50  text-white flex justify-between items-center bg-brand-dark py-4 px-6">
-                <a href="#" className="font-black text-2xl tracking-tighter flex items-center gap-2 transition-colors">
-                    <Logo />
-                    <p>Tramita</p>
-                </a>
+        <header className="sticky top-0 z-50 text-white flex justify-between items-center bg-brand-dark py-4 px-4 md:px-10">
+            <a href="#" className="font-black text-xl sm:text-2xl tracking-tighter flex items-center gap-2 transition-colors">
+                <Logo />
+                <p>Tramita</p>
+            </a>
 
-                <div className="flex items-center gap-5">
-                    <Button variant="ghost">Entrar</Button>
-                    <Button>Cadastra-se Grátis</Button>
-                </div >
-            </header >
-        </>
+            <div className="flex items-center gap-2 md:gap-5">
+                <Button variant="ghost" className="px-2">Entrar</Button>
+                <Button className="px-3 md:px-5 py-2 md:py-2.5 text-xs sm:text-[15px]">Cadastre-se<span className="hidden sm:inline"> Grátis</span></Button>
+            </div >
+        </header >
     )
 }
 
